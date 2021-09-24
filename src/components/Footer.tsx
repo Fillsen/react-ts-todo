b53edd7c0ca196538ce34cebb765c54ff7ed0b6a
Max Fillsen <51379917+Fillsen@users.noreply.github.com>
2021-09-24 16:07:44 +0300

    oops

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,19 @@
 import React, {FC} from 'react';
 import '../App.css'
 
-const Footer:FC = () => {
+const Footer: FC = () => {
     return (
         <footer className="page-footer footer blue lighten-1">
-                <div className="container">
-                    &copy; by Fillsen 2021
-                    <a className="grey-text text-lighten-4 right" href="/">Project Repo</a>
-                </div>
+            <div className="container">
+                &copy; by Fillsen 2021
+                <a
+                    className="grey-text text-lighten-4 right"
+                    href="https://github.com/Fillsen/react-ts-todo"
+                    target='_blank'
+                    rel="noreferrer">
+                    Project Repo
+                </a>
+            </div>
         </footer>
     );
 };
